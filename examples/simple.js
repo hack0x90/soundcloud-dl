cf337ad90b9348367d5cc534ed1cb244fb07e898
hack0x90 <jahan@hack0x90.com>
2022-06-18 22:59:33 +0500

    Added support for proxy when using as an npm module

--- examples/simple.js
+++ examples/simple.js
@@ -3,7 +3,8 @@ const getSoundCloudTrackLink = require('../');
 async function getLink () {
   const link = await getSoundCloudTrackLink({
     scTrackUrl: 'https://soundcloud.com/rashidaliofficial/bhangra-loco',
-    scClientId: process.env.SC_CLIENT_ID
+    scClientId: process.env.SC_CLIENT_ID,
+    proxy: process.env.SC_PROXY
   });
   console.log('------------ Link ------------');
   console.log(link);
